2aca484717d0642e9afc13bf3fe31953eb7510f9
francelysl <flomeli@hawk.iit.edu>
2025-08-17 17:57:33 -0500

    Resolving module errors, Setup of initial components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
