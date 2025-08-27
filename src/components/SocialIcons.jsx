5ffb1ea988481072a02a8a30ada82ade33f19427
francelysl <flomeli@hawk.iit.edu>
2025-08-27 16:02:03 -0500

    Updating cursor interaction, Removing grey background surrounding icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -3,24 +3,28 @@ import "../styles/SocialIcons.css";
 
 export default function SocialIcons() {
   return (
-    <div className="social-container">
+    <div className="social-container" aria-label="Social links">
       <div className="icon-menu">
-        <a href="mailto:francelyslomeli@gmail.com">
-          <i className="fa-solid fa-envelope"></i>
+        <a href="mailto:francelyslomeli@gmail.com" title="Email" aria-label="Email">
+          <i className="fa-solid fa-envelope" />
         </a>
         <a
           href="https://www.linkedin.com/in/francelyslomeli/"
           target="_blank"
           rel="noopener noreferrer"
+          title="LinkedIn"
+          aria-label="LinkedIn"
         >
-          <i className="fa-brands fa-linkedin-in"></i>
+          <i className="fa-brands fa-linkedin-in" />
         </a>
         <a
           href="https://github.com/francelysl"
           target="_blank"
           rel="noopener noreferrer"
+          title="GitHub"
+          aria-label="GitHub"
         >
-          <i className="fa-brands fa-github"></i>
+          <i className="fa-brands fa-github" />
         </a>
       </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "@fortawesome/fontawesome-free/css/all.min.css"; // use npm FA
+import "@fortawesome/fontawesome-free/css/all.min.css"; 
 
 const root = createRoot(document.getElementById("root"));
 root.render(
