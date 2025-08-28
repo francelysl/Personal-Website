2bae6b67dd7c5e0d95e555ab638072787c41dfe1
francelysl <flomeli@hawk.iit.edu>
2025-08-27 21:35:22 -0500

    Refactoring of CSS in education section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SocialIcons from "../components/SocialIcons";
+import ExperienceTimeline from "../components/ExperienceTimeline";
+import EducationItem from "../components/Education";
 
 export default function Home() {
   return (
-    <section id="home">
-      <h1>Welcome to My Portfolio</h1>
-      <p>...</p>
+    <section id="home"style={{ padding: "32px 0" }}>
+
+      <ExperienceTimeline />
       <SocialIcons />
+      <EducationItem />
     </section>
   );
 }
