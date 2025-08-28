179fa31c5e1056e041631a39bb92e0944a6992a3
francelysl <flomeli@hawk.iit.edu>
2025-08-27 21:48:08 -0500

    Fixing indentation component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import SocialIcons from "../components/SocialIcons";
-import ExperienceTimeline from "../components/ExperienceTimeline";
-import EducationItem from "../components/Education";
+import EducationItem from "../components/EducationItem";
+import "../styles/EducationItem.css";
 
 export default function Home() {
   return (
     <section id="home"style={{ padding: "32px 0" }}>
-
-      <ExperienceTimeline />
       <SocialIcons />
       <EducationItem />
     </section>
